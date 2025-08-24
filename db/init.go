b9a746f395240aa5e9c1ad74c3aad3610926820b
NAbinav <abinav0506@gmail.com>
2025-08-24 08:51:24 +0530

    using pgxpool for concurrent access

--- db/init.go
+++ db/init.go
@@ -3,17 +3,17 @@ package db
 import (
 	"context"
 	"fmt"
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"os"
 )
 
-var DB *pgx.Conn
+var DB *pgxpool.Pool
 
 type Dummy []any
 
 func Init_DB() {
 	var err error
-	DB, err = pgx.Connect(context.Background(), os.Getenv("PSQL_URL"))
+	DB, err = pgxpool.New(context.Background(), os.Getenv("PSQL_URL"))
 	fmt.Println(os.Getenv("PSQL_URL"))
 	if err != nil {
 		fmt.Print(err)
